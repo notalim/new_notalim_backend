ea85d70ff1f60a02e9c8a05cd3660d36c075ab86
notalim <alimneedstowork@gmail.com>
2024-03-21 19:47:17 -0400

    cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import setupRoutes from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 setupRoutes(app);
 
