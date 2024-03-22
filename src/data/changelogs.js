e475496b5a5673cb67269db5f349641df462f5a3
notalim <alimneedstowork@gmail.com>
2024-03-21 21:49:33 -0400

    mongo for changelogs updated

--- src/data/changelogs.js
+++ src/data/changelogs.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { changelogs } from "../config/mongoCollections.js";
 
 export async function getLastTenChangelogs() {
@@ -7,4 +8,20 @@ export async function getLastTenChangelogs() {
         .sort({ dateTime: -1 })
         .limit(10)
         .toArray();
+}
+
+export async function insertChangelog(changelogData) {
+    const changelogsCollection = await changelogs();
+    const insertInfo = await changelogsCollection.insertOne(changelogData);
+    if (insertInfo.insertedCount === 0) throw "Could not add changelog";
+    return insertInfo.insertedId; 
+}
+
+export async function checkIfChangelogExists(identifier) {
+    const changelogsCollection = await changelogs();
+    const query = {
+        $or: [{ "commit.sha": identifier }, { "pull_request.id": identifier }],
+    };
+    const changelog = await changelogsCollection.findOne(query);
+    return changelog !== null; 
 }
